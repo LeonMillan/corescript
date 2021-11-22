98c76134befebac3d235ac8e71faa04da5c6ac53
LeonMillan <nookleun@gmail.com>
2021-11-22 01:59:04 -0300

    watch

--- watch.js
+++ watch.js
@@ -1,16 +1,23 @@
-const {readFileSync} = require('fs');
-
-const {copySync} = require('cpx');
+const cpx = require('cpx');
+const { readFileSync } = require('fs');
 const watch = require('node-watch');
 
 const concatSource = require('./concat.js');
 
+const root = './game/www';
+
 JSON.parse(readFileSync('./modules.json').toString())
-    .map(name=>({name, target:`./js/${name}`}))
-    .forEach(({name,target})=>{
-        watch(target, ()=>{
+    .map(name => ({ name, target: `./js/${name}` }))
+    .forEach(({ name, target }) => {
+        watch(target, () => {
             concatSource(name);
-            copySync(`./dist/${name}.js`, './game/js/');
+
+            cpx.copySync('./template/**/*', root, { update: true });
+            cpx.copySync('./dist/*', root + '/js', { update: true });
+            cpx.copySync('./js/libs/*', root + '/js/libs', { update: true });
+            cpx.copySync('./js/extras_img/*', root + '/img/system', { update: true });
+            cpx.copySync('./plugins/*', root + '/js/plugins', { update: true });
+
             console.log(`${target} changed. copy ${name}.js`);
         });
     });
