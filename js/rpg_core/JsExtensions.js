7f3728e557cade79247c9b9da56c3a0a6fbb0ac9
LeonMillan <nookleun@gmail.com>
2023-04-26 08:38:52 -0300

    Array extensions

--- js/rpg_core/JsExtensions.js
+++ js/rpg_core/JsExtensions.js
@@ -73,56 +73,106 @@ Number.prototype.padZero = function(length){
 };
 
 Object.defineProperties(Array.prototype, {
-    /**
-     * Checks whether the two arrays are same.
-     *
-     * @method Array.prototype.equals
-     * @param {Array} array The array to compare to
-     * @return {Boolean} True if the two arrays are same
-     */
-    equals: {
-        enumerable: false,
-        value: function(array) {
-            if (!array || this.length !== array.length) {
-                return false;
-            }
-            for (var i = 0; i < this.length; i++) {
-                if (this[i] instanceof Array && array[i] instanceof Array) {
-                    if (!this[i].equals(array[i])) {
-                        return false;
-                    }
-                } else if (this[i] !== array[i]) {
-                    return false;
-                }
-            }
-            return true;
+  /**
+   * Checks whether the two arrays are same.
+   *
+   * @method Array.prototype.equals
+   * @param {Array} array The array to compare to
+   * @return {Boolean} True if the two arrays are same
+   */
+  equals: {
+    enumerable: false,
+    value: function (array) {
+      if (!array || this.length !== array.length) {
+        return false;
+      }
+      for (var i = 0; i < this.length; i++) {
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+          if (!this[i].equals(array[i])) {
+            return false;
+          }
+        } else if (this[i] !== array[i]) {
+          return false;
         }
+      }
+      return true;
     },
-    /**
-     * Makes a shallow copy of the array.
-     *
-     * @method Array.prototype.clone
-     * @return {Array} A shallow copy of the array
-     */
-    clone: {
-        enumerable: false,
-        value: function() {
-            return this.slice(0);
-        }
+  },
+  /**
+   * Makes a shallow copy of the array.
+   *
+   * @method Array.prototype.clone
+   * @return {Array} A shallow copy of the array
+   */
+  clone: {
+    enumerable: false,
+    value: function () {
+      return this.slice(0);
     },
-    /**
-     * Checks whether the array contains a given element.
-     *
-     * @method Array.prototype.contains
-     * @param {Any} element The element to search for
-     * @return {Boolean} True if the array contains a given element
-     */
-    contains : {
-        enumerable: false,
-        value: function(element) {
-            return this.indexOf(element) >= 0;
+  },
+  /**
+   * Checks whether the array contains a given element.
+   *
+   * @method Array.prototype.contains
+   * @param {Any} element The element to search for
+   * @return {Boolean} True if the array contains a given element
+   */
+  contains: {
+    enumerable: false,
+    value: function (element) {
+      return this.indexOf(element) >= 0;
+    },
+  },
+  /**
+   * Rotate a 2D table, switching the X and Y axis.
+   * Axis dimensions are inferred by the first element of the array.
+   *
+   * @method Array.prototype.rotate
+   * @return {Any[]} the resulting rotated 2D table
+   */
+  rotate: {
+    enumerable: false,
+    value: function () {
+      if (!Array.isArray(this[0])) throw new TypeError("Array is not 2D");
+      const xsize = this.length;
+      const ysize = this[0].length;
+      let result = new Array(ysize);
+      for (let y = 0; y < ysize; y++) {
+        result[y] = [];
+        for (let x = 0; x < xsize; x++) {
+          result[y][x] = this[x][y];
         }
-    }
+      }
+      return result;
+    },
+  },
+  /**
+   * Transform the array into a multi-dimensional table using the provided dimensions list.
+   *
+   * @method Array.prototype.unflatten
+   * @param {number[]} dimensions List of dimensions to be used
+   * @return {Any[]} the resulting multi-dimensional table
+   */
+  unflatten: {
+    enumerable: false,
+    value: function (dimensions) {
+      if (!Array.isArray(dimensions))
+        throw new TypeError("Must provide array of dimensions as parameter");
+      const sigma = dimensions.reduce((total, curr) => total * curr, 1);
+      if (this.length % sigma !== 0)
+        throw new Error("Array cannot be split with the provided dimensions");
+      let result = [...this];
+      for (let dim = dimensions.length - 1; dim >= 0; dim--) {
+        result = result.reduce((arr, item, index) => {
+          const pos = Math.floor(index / dimensions[dim]);
+          if (!arr[pos]) arr[pos] = [];
+          arr[pos].push(item);
+          return arr;
+        }, []);
+      }
+      return result;
+    },
+  },
 });
 
 /**
